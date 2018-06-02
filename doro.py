d2b2988cffb7c07c7047b3b98fb347c5e71867fd
dorcasandayi <dorcasandayi@gmail.com>
2018-06-02 10:52:29 +0300

    first commit

---
+++ doro.py
@@ -0,0 +1,17 @@
+list 1 =[1,2,3,4,]
+print (list 1)
+
+list 2 =["dijo","doro","loryne"]
+print (list 2)
+
+list 3 =["dijo","doro",7,8]
+print (list 3)
+
+tuple = ("nairobi","kisumu",48,56)
+
+first_list = [1,2,"dijo","doro","loryne",4,8]
+first_set = (first_list)
+print (first_set)
+
+dict{"name":"dijo","age":20,"address":"nairobi"}
+print (dict)
